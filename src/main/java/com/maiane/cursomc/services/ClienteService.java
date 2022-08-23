829e31cf9b07f3f7fb4e0b14435f56f939f90083
MaianeSamara <maianesamara@gmail.com>
2022-08-23 16:52:10 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/maiane/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.maiane.cursomc.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maiane.cursomc.domain.Cliente;
+import com.maiane.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! id: " + id + ", Tipo: " + Cliente.class.getName(), null));
+		
+		
+	}
+	
+}
