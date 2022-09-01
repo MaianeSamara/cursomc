ba5f6f47b5f5aec5c7228b25e1589776e4ae0ef9
MaianeSamara <maianesamara@gmail.com>
2022-09-01 18:49:51 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/maiane/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.maiane.cursomc.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maiane.cursomc.domain.Pedido;
+import com.maiane.cursomc.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! id: " + id + ", Tipo: " + Pedido.class.getName(), null));
+
+	}
+
+}
